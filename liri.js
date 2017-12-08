25469352d3c66186fcc0fe89673b41a14df5b968
akaryanreynolds <ryanreynolds.ca@gmail.com>
2017-12-08 10:32:05 -0800

    Working on Spotify
    Trying to get Spotify to work.

--- liri.js
+++ liri.js
@@ -5,17 +5,17 @@ var fs = require("fs");
 var action = process.argv[2];
 var value = process.argv[3];
 
-var client = new twitter({
-    consumer_key: keys.twitterKeys.consumer_key,
-    consumer_secret: keys.twitterKeys.consumer_secret,
-    access_token_key: keys.twitterKeys.access_token_key,
-    access_token_secret: keys.twitterKeys.access_token_secret
-  })
+// var client = new twitter({
+//     consumer_key: keys.twitterKeys.consumer_key,
+//     consumer_secret: keys.twitterKeys.consumer_secret,
+//     access_token_key: keys.twitterKeys.access_token_key,
+//     access_token_secret: keys.twitterKeys.access_token_secret
+//   })
 
-var params = {
-    screen_name: 'akaryanreynolds'
-    count: 10
-}
+// var params = {
+//     screen_name: 'akaryanreynolds'
+//     count: 10
+// }
 
 // From class activity 15 converted to work with hw
 switch (action) {
@@ -36,7 +36,27 @@ switch (action) {
       break;
   }
 
-// functions
-function myTweets(){
+// // functions
+// function myTweets(){
     
-}
+// }
+
+function spotifyThis(value){
+  if(value === null){
+    value = 'The Sign';
+  }
+  request('https://api.spotify.com/v1/search?q=' + value + '&type=track', function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        jsonBody = JSON.parse(body);
+        console.log(' ');
+        console.log('Artist: ' + jsonBody.tracks.items[0].artists[0].name);
+        console.log('Song: ' + jsonBody.tracks.items[0].name);
+        console.log('Preview Link: ' + jsonBody.tracks.items[0].preview_url);
+        console.log('Album: ' + jsonBody.tracks.items[0].album.name);
+        console.log(' ');
+        fs.appendFile('terminal.log', ('=============== LOG ENTRY BEGIN ===============\r\n' + Date() +'\r\n \r\nTERMINAL COMMANDS:\r\n$: ' + process.argv + '\r\n \r\nDATA OUTPUT:\r\n' + 'Artist: ' + jsonBody.tracks.items[0].artists[0].name + '\r\nSong: ' + jsonBody.tracks.items[0].name + '\r\nPreview Link: ' + jsonBody.tracks.items[0].preview_url + '\r\nAlbum: ' + jsonBody.tracks.items[0].album.name + '\r\n=============== LOG ENTRY END ===============\r\n \r\n'), function(err) {
+            if (err) throw err;
+        });
+      }
+    });
+  }
